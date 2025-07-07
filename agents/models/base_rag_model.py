48f7277eb0b2d83e8224b93083557260b258c94c
Jordan <jordianojr2001@gmail.com>
2025-07-08 02:44:25 +0800

    changed prompt

--- agents/models/base_rag_model.py
+++ agents/models/base_rag_model.py
@@ -1,4 +1,4 @@
 import ollama
 
 # model that I used most for RAG
-model = ollama.create(model='student_agent', from_='llama3.2:3b', system="You are a sophomore Computer Information Systems undergraduate who makes mistakes sometimes.")
+model = ollama.create(model='student_agent', from_='llama3.2:3b', system="You are a sophomore Computer Information Systems undergraduate.")
